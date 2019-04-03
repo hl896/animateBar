58e730ba494cd7b4c8115e591e3b4e7496114b44
yeung.li <yeung.li@hinterlands.tech>
2019-04-03 15:44:29 +1100

    2019 04 03 3pm

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Bar >
+        <Bar value_status1st='20'
+            value_status2nd='50'
+            value_status3th='80'>
 
         </Bar> 
       </div>
--- src/Bars/Bar.jsx
+++ src/Bars/Bar.jsx
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import './bar.css';
-
+import {Form} from 'react-bootstrap'
 class Bar extends Component{
     constructor(props){
         super(props);
@@ -11,15 +11,29 @@ class Bar extends Component{
             value_status3th:70
         }
     }
-    
-    
-        
     render(){
         return (
-            <div>
-                1.<ProgressBar animated className='singleBar' striped variant="success" now={this.state.value_status1st} />
-                2.<ProgressBar animated className='singleBar' striped variant="success" now={this.state.value_status2nd} />
-                3.<ProgressBar animated className='singleBar' striped variant="success" now={this.state.value_status3th} />
+            <div >
+                <div>
+                    <ProgressBar animated className='singleBar' id='first' striped variant="success" now={this.state.value_status1st} label={`${this.props.value_status1st}%`}/>
+                    <ProgressBar animated className='singleBar' id='second' striped variant="warning" now={this.state.value_status2nd} label={`${this.state.value_status2nd}%`}/>
+                    <ProgressBar animated className='singleBar' id='third' striped variant="info" now={this.state.value_status3th} label={`${this.props.value_status3th}%`}/>
+                </div>
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Example select</Form.Label>
+                        <Form.Control as="select">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <button>-25</button>
+                    <button>-10</button>
+                    <button>10</button>
+                    <button>25</button>
+                </Form>
+                
             </div>
             
         );
