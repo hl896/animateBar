d12245f4964b148ac2071959d69843166e54571d
yeung.li <yeung.li@hinterlands.tech>
2019-04-29 22:01:37 +1000

    Updates

---
+++ precache-manifest.4526960c646dbb8cf1cdbf1e071d3275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animateBar/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "76ca27c443fc114d2887",
+    "url": "/animateBar/static/js/runtime~main.74e2df3b.js"
+  },
+  {
+    "revision": "3fd03db7e7e32207513c",
+    "url": "/animateBar/static/js/main.a0352ba6.chunk.js"
+  },
+  {
+    "revision": "1eef88e6b83a2214aaa7",
+    "url": "/animateBar/static/js/2.e3341f7a.chunk.js"
+  },
+  {
+    "revision": "3fd03db7e7e32207513c",
+    "url": "/animateBar/static/css/main.51542823.chunk.css"
+  },
+  {
+    "revision": "3cd6f84031dace1ec0f4e6c0c62cea7b",
+    "url": "/animateBar/index.html"
+  }
+];
